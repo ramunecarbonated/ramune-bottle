7bd9e3c08c54d1dd2a65de4015764b5689f1e75f
Sascha <sascha@ramune.me>
2018-06-11 13:28:30 +0200

    Improve testing, add config,js for 'public' variables

--- test.js
+++ test.js
@@ -2,9 +2,10 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 var env = process.env;
+var sasch = "77552496561631232";
 
 client.login( env.TOKEN );
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    client.users.get(sasch).send('Travis-CI integration success.').catch(console.error);
 });
