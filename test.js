e0aeea9d6f77427698ed6accd5a35c6ce58754ca
Sascha <sascha@ramune.me>
2018-06-11 15:10:19 +0200

    improve test.js to actually be succesful for travis build

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 var env = process.env;
-var sasch = "77552496561631232";
+
+var config = require('./config.js');
 
 client.login( env.TOKEN );
 
 client.on('ready', () => {
-    client.users.get(sasch).send('Travis-CI integration success.').catch(console.error);
+    client.users.get(config.sasch).send('Travis-CI integration success.')
+        .then(console.log(`Logged in as ${client.user.tag}!`))
+        .catch(console.error);
 });
