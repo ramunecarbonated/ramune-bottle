e6eeeaa532ebc3fe1830bd906777917189267af4
Sascha <sascha@ramune.me>
2018-06-21 11:47:21 +0200

    move to include folder

--- config.js
+++
@@ -1,5 +0,0 @@
-exports.sasch = "77552496561631232";
-exports.cooldown = 6000;
-exports.prefix = "-";
-exports.temp = "temp";
-exports.cleanTime = 10;
---
+++ include/config.js
@@ -0,0 +1,5 @@
+exports.sasch = "77552496561631232";
+exports.cooldown = 6;
+exports.prefix = "+";
+exports.temp = "temp";
+exports.cleanTime = 60;
