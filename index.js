70151caf909454579f3dbb5e6463fcf5cf362680
Sascha <sascha@ramune.me>
2018-07-24 09:37:24 +0200

    oopsie woopsie

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ client.on('message', msg => {
             }
             // if postUrl exists
             if (typeof c.postUrl !== 'undefined' && c.postUrl) {
-                if (arr['image'].length < 1) throw "couldn't find an image to use, you need to directly upload an image. (i can use that and any previous images without requiring an upload after)";
+                if ("image" in arr && arr['image'].length < 1) throw "couldn't find an image to use, you need to directly upload an image. (after doing so I can use that and any previous images posted in the chat)";
                 // TODO: move this to seperate module
                 request.post({
                     followAllRedirects: true,
